1afede14930837044b72b78e0fbd4d7febc5c7ab
Hadi Abou Hawili <aboualhood2005@hotmail.com>
2026-02-21 20:22:52 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -27,4 +27,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  console.log("API URL:", import.meta.env.VITE_API_URL);
 });
